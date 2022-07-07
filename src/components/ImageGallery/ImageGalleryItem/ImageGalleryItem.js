5370f37c137e679b57d4bc910e692e05b32171e3
Maksym Kundeev <mkundeev@gmail.com>
2022-07-07 23:08:22 +0300

    imageitem to rfc

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -1,23 +1,17 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import s from './ImageGalleryItem.module.css'
+import s from './ImageGalleryItem.module.css';
 
+export default function ImageGalleryItem({ url, data, onClick }) {
+  const onOpen = e => {
+    onClick(data);
+  };
 
-class ImageGalleryItem extends React.Component{
-   
-
-    onOpen = e => {
-        this.props.onClick(this.props.data)
-    }
-
-    render() {   
-       return <img src={this.props.url} alt="" className={s.galleryImg} onClick={this.onOpen} />
-    }
+  return <img src={url} alt="" className={s.galleryImg} onClick={onOpen} />;
 }
-ImageGalleryItem.propTypes = {
-    url: PropTypes.string.isRequired,
-    data: PropTypes.string.isRequired,
-    onClick:PropTypes.func.isRequired,
-}  
 
-export default ImageGalleryItem
+ImageGalleryItem.propTypes = {
+  url: PropTypes.string.isRequired,
+  data: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
